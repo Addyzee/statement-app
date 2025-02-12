39bb145b5a5d7004c708e495dd3eb1feb209d88b
Addyzee <andrewkamaukim@gmail.com>
2025-02-12 12:33:00 +0300

    replace pikepdf

--- decrypt.py
+++ decrypt.py
@@ -1,5 +1,27 @@
-import pikepdf
+import os
+from pypdf import PdfReader, PdfWriter
+
 
 def remove_password_from_pdf(filename, output_name, password=None):
-    pdf = pikepdf.open(filename, password=password)
-    pdf.save(f"output/{output_name}.pdf")
+    try:
+        reader = PdfReader(filename)
+
+        if reader.is_encrypted:
+            if not reader.decrypt(password):  
+                raise ValueError("Incorrect password or decryption failed.")
+
+        writer = PdfWriter(clone_from=reader)
+
+        output_dir = "output"
+        os.makedirs(output_dir, exist_ok=True)
+
+        output_path = os.path.join(output_dir, output_name)
+
+        with open(output_path, "wb") as f:
+            writer.write(f)
+
+    except Exception as e:
+        print(f"Error: {e}")
+
+
+
