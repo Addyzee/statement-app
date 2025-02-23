8fca16317024b7d39692ab9f664105cc1f159bec
Addyzee <andrewkamaukim@gmail.com>
2025-02-23 13:50:46 +0300

    transaction type api data structuring

--- backend/app/analysis/t_type_analysis.py
+++ backend/app/analysis/t_type_analysis.py
@@ -3,7 +3,8 @@ from .totals import total_cashflow
 from .utils import convert_to_default
 from typing import Dict, List, Union
 
-def get_transaction_types(data: pd.DataFrame)-> List[str]:
+
+def get_transaction_types(data: pd.DataFrame) -> List[str]:
     return list(data["Type"].unique())
 
 
@@ -16,15 +17,13 @@ def amount_outin_per_transaction_type(
 ) -> Dict[str, Dict[str, float]]:
 
     transaction_types = convert_to_default(
-        data=data,
-        object=transaction_types, 
-        defaulter_function=get_transaction_types
+        data=data, object=transaction_types, defaulter_function=get_transaction_types
     )
 
     if not transaction_types:
         raise ValueError("No transactions")
 
-    mapped_amounts = {"In": {}, "Out": {}}
+    mapped_amounts = {"In": [], "Out": []}
 
     # Filter data before grouping to avoid unnecessary processing
     filtered_data = data[data["Type"].isin(transaction_types)]
@@ -34,18 +33,23 @@ def amount_outin_per_transaction_type(
         total = total_cashflow(data=filtered_data, direction=dir)
 
         if total == 0:
-            continue  
+            continue
+        mapped_amounts[dir] = []
+        others = {"Others": []}
 
         for transaction_type, df in grouped_data:
             amount = total_cashflow(data=df, direction=dir)
             percent = (amount / total * 100) if total > 0 else 0
-
+            attrs = {"Type": transaction_type, "Amount": amount}
             if amount > 0 and percent >= 7:
-                mapped_amounts[dir][transaction_type] = amount
-            elif amount > 0 and percent < 7:
-                mapped_amounts[dir].setdefault("Others", {})[transaction_type] = amount
+                mapped_amounts[dir].append(attrs)
 
-        if "Others" in mapped_amounts[dir]:
-            mapped_amounts[dir]["Others"]["Total"] = sum(mapped_amounts[dir]["Others"].values())
+            elif amount > 0 and percent < 7:
+                others["Others"].append(attrs)
+
+        if len(others["Others"]) > 0:
+            total_others = sum([attr["Amount"] for attr in others["Others"]])
+            mapped_amounts[dir].append({"Type": "Others", "Amount": total_others})
+            mapped_amounts[dir].append(others)
 
     return mapped_amounts
