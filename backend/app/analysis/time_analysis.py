b33d579d479fd882371b3b34a21dc9e052437002
Addyzee <andrewkamaukim@gmail.com>
2025-02-23 15:48:10 +0300

    time analysis api restructure

--- backend/app/analysis/time_analysis.py
+++ backend/app/analysis/time_analysis.py
@@ -1,13 +1,15 @@
 import pandas as pd
-from .totals import total_cashflow
 
 
 def total_outin_by_month(data: pd.DataFrame):
-    grouped_data = data.groupby(pd.Grouper(key='Date', axis=0,  freq='ME'))
-    mapped_amounts = {}
-    
+    grouped_data = data.groupby(pd.Grouper(key="Date", axis=0, freq="ME"))
+    mapped_amounts = []
+
     for date, df in grouped_data:
         month_year = date.strftime("%b-%Y")
-        amounts = total_cashflow(df)
-        mapped_amounts[month_year] = amounts
+        amounts_in = df.loc[df["Direction"] == "In", "Amount"].sum()
+        amounts_out = df.loc[df["Direction"] == "Out", "Amount"].sum()
+        mapped_amounts.append({"Month": month_year,
+                               "In": amounts_in,
+                               "Out":amounts_out})
     return mapped_amounts
